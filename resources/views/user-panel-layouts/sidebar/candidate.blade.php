14ff021942b826c3b9cba04f5fbff8def4df863c
Ashish Dake <dakeashish1997@gmail.com>
2025-05-28 04:19:33 +0530

    unwanted files deleted

--- resources/views/user-panel-layouts/sidebar/candidate.blade.php
+++
@@ -1,6 +0,0 @@
-<li class="nav-item">
-    <a href="{{route('candidate.practiceTest.index')}}" class="nav-link">
-        <i class="nav-icon far fa-circle"></i>
-        <p>Practice Test</p>
-    </a>
-</li>
--- resources/views/user-panel-layouts/sidebar/freelancer.blade.php
+++
@@ -1,6 +0,0 @@
-<li class="nav-item">
-    <a href="{{route('freelancer.questions.index')}}" class="nav-link">
-        <i class="nav-icon far fa-circle"></i>
-        <p>Freelancer Question</p>
-    </a>
-</li>
